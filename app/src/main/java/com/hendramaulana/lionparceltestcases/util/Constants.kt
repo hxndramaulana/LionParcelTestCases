3acabe8d859b5aff391288ac9a8d0e5f9760ae82
Hendra Maulana <hendra_maulana@outlook.com>
2022-05-19 18:55:51 +0700

    retrofit setup

--- app/src/main/java/com/hendramaulana/lionparceltestcases/util/Constants.kt
+++ app/src/main/java/com/hendramaulana/lionparceltestcases/util/Constants.kt
@@ -1,7 +1,8 @@
 package com.hendramaulana.lionparceltestcases.util
 
 class Constants {
-    companion object{
-        const val API_KEY= "e377298cf8244cfa97df1523aac123fc"
+    companion object {
+        const val API_KEY = "e377298cf8244cfa97df1523aac123fc"
+        const val BASE_URL = "newsapi.org"
     }
 }
